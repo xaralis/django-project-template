2baf27f7cca7e177090ee2b75ac255c4575dfebc
xaralis <filip.varecha@fragaria.cz>
2012-04-12 09:44:51 +0200

    Removed DB root

--- %(project)s/%(project)s/settings/config.py
+++ %(project)s/%(project)s/settings/config.py
@@ -9,7 +9,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': '%(project)s',
-        'USER': 'root',
+        'USER': '',
         'PASSWORD': '',
     },
 }
